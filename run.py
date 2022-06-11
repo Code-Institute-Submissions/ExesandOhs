0e5d6b59c548a5c97159c3d1a609e8d0e0f87602
Katagaria <vonvix@hotmail.com>
2022-06-11 01:08:32 +0000

    Add username input

--- run.py
+++ run.py
@@ -1,14 +1,24 @@
+# player name
+playerName = input("Your letter is X. What is your first name?:")
+print("Welcome to Exes and Ohs, " + playerName)
 # print the board
 
-board = ["-","-","-",
-        "-","-","-",
-        "-","-","-"]
+board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+
+print(board[0:3])
+print(board[3:6])
+print(board[6:])
+
+count = 0
 
 # play game
+moves = int(input("Place X anywhere between 1-9"))
 
+if board[moves-1] == "-":
+    board[moves-1] = "x"
+    count = count + 1
 
 # player input
-
 
 # check for a win or a tie
 
